68b061c0416099c4703bcaac2df018dfe4a70758
cooleo273 <leulteferi273@gmail.com>
2024-04-17 18:46:53 +0100

    nav bar on pricing

---
+++ src/Screens/Pricing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pricing() {
+  return (
+    <div>
+        <div>
+        <div className="circletwo"></div>
+      </div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Pricing
